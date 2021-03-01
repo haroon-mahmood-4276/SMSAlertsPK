67a1991bd4abb250f4ae8e7150d28b763ff9fc66
Haroon Mahmood <70590766+haroon-mahmood-4276@users.noreply.github.com>
2021-03-01 18:03:36 +0500

    SomeFixes

--- resources/views/shared/layout.blade.php
+++ resources/views/shared/layout.blade.php
@@ -6,17 +6,20 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>@yield('PageTitle') - SMS4Everyone</title>
-
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-        <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
-        <link rel="stylesheet" href="{{ asset('css/app.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/authentication.css') }}">
-        {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
-            integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous"> --}}
+
+
     </head>
 
     <body>
-        <div id="app">
+        <div class="main-wrapper" id="main-wrapper">
+            <div class="preloader">
+                <div class="loader">
+                    <div class="loader__figure"></div>
+                    <p class="loader__label">SMS 4 Every</p>
+                </div>
+            </div>
+
             <div>
                 {{ View::make('shared.header') }}
             </div>
@@ -29,12 +32,6 @@
             {{ View::make('shared.footer') }}
         </div>
 
-       
-        <script src="{{ asset('js/jquery.min.js') }}"></script>
-        {{-- <script src="{{ asset('js/app.js') }}"></script> --}}
-        {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous">
-        </script> --}}
     </body>
 
 </html>
