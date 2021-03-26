205b2ffbce6f6b2b9caf4c2acdc710f349163f23
Haroon Mahmood <70590766+haroon-mahmood-4276@users.noreply.github.com>
2021-03-26 08:58:27 +0500

    Template Controller

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\GroupController;
 use App\Http\Controllers\MobileDataController;
 use App\Http\Controllers\SectionController;
+use App\Http\Controllers\TemplateController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,13 +27,15 @@ Route::post('login', [UserController::class, 'login'])->name('r.login');
 
 Route::group(['middleware' => ['AuthRoute']], function () {
     Route::get('login', [UserController::class, 'loginform'])->name('r.login');
-    
-    
 });
 Route::get('dashboard', [UserController::class, 'dashboard'])->name('r.dashboard');
 
-Route::resource('users', UserController::class);
-Route::resource('groups', GroupController::class);
-Route::resource('sections', SectionController::class);
-Route::resource('data', MobileDataController::class);
+Route::resources([
+    'users' => UserController::class,
+    'groups' => GroupController::class,
+    'sections' => SectionController::class,
+    'data' => MobileDataController::class,
+    'templates' => TemplateController::class
+]);
+
 Route::get('logout', [UserController::class, 'logout'])->name('r.logout');
