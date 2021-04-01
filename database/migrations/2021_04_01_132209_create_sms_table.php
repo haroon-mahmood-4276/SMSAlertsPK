8cd548a2b792a6fbe6ee737e287fcad7a6974e90
Haroon Mahmood <haroon.mahmood.4276@gmail.com>
2021-04-01 22:01:02 +0500

    Sms Table

---
+++ database/migrations/2021_04_01_132209_create_sms_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSmsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sms', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->unsignedBigInteger('data_id')->nullable();
+            $table->id();
+            $table->string('sms');
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('data_id')->references('id')->on('mobiledatas');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sms');
+    }
+}
