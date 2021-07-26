89ae6142bb37efd8eb80915e0f2f930142ae905d
haroon-mahmood-4276 <haroon.mahmood.4276@gmail.com>
2021-07-26 12:18:12 +0500

    Quick & Multiple SMS Working

--- app/Jobs/JobSendSms.php
+++ app/Jobs/JobSendSms.php
@@ -9,9 +9,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Str;
-
 class JobSendSms implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
