f663b08e4fc7f13e8c3ab8d5716503a7c8e0f7ec
haroon-mahmood-4276 <haroon.mahmood.4276@gmail.com>
2021-07-30 21:29:11 +0500

    Update Sms Settings to Database

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -29,15 +29,15 @@ class SettingController extends Controller
 
     public function SMSSetting(Request $request)
     {
-        return $request->input();
-        $BirthdaySetting = Setting::find(session('Data.id'));
-        $BirthdaySetting->birthday_enabled = ($request->is_enabled == 'on') ? 'Y' : 'N';
-        $BirthdaySetting->birthday_message = ($request->has('message')) ? $request->message : null;
+        $SMSSetting = Setting::find(session('Data.id'));
+        $SMSSetting->primary_number_2 = ($request->primary_number_2 == 'on') ? 'Y' : 'N';
+        $SMSSetting->secondary_number_1 = ($request->secondary_number_1 == 'on') ? 'Y' : 'N';
+        $SMSSetting->secondary_number_2 = ($request->secondary_number_2 == 'on') ? 'Y' : 'N';
 
-        if ($BirthdaySetting->save()) {
-            return redirect()->route('r.settings')->with('AlertType', 'success')->with('AlertMsg', 'Birthday setting saved.');
+        if ($SMSSetting->save()) {
+            return redirect()->route('r.settings')->with('AlertType', 'success')->with('AlertMsg', 'Sms setting saved.');
         } else {
-            return redirect()->route('r.settings')->with('AlertType', 'success')->with('AlertMsg', 'Birthday setting saved.');
+            return redirect()->route('r.settings')->with('AlertType', 'success')->with('AlertMsg', 'Sms setting saved.');
         }
     }
 }
