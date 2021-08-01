91ad234d8edab6f10221d8d87e60749663d60106
haroon-mahmood-4276 <haroon.mahmood.4276@gmail.com>
2021-08-01 18:15:21 +0500

    Lock All the Routes

--- routes/web.php
+++ routes/web.php
@@ -37,105 +37,106 @@ Route::post('login', [UserController::class, 'login'])->name('r.login');
 
 Route::group(['middleware' => ['AuthRoute']], function () {
     Route::get('login', [UserController::class, 'loginform'])->name('r.login');
-});
-Route::get('dashboard', [UserController::class, 'dashboard'])->name('r.dashboard');
 
-Route::resources([
-    'users' => UserController::class,
-    'groups' => GroupController::class,
-    'sections' => SectionController::class,
-    'data' => MobileDataController::class,
-    'templates' => TemplateController::class,
-]);
+    Route::get('dashboard', [UserController::class, 'dashboard'])->name('r.dashboard');
 
-Route::get('sections/{section}/list', [SectionController::class, 'GetSectionList'])->name('r.sectionlist');
-Route::get('data/{groupid}/{sectionid}/list', [MobileDataController::class, 'STDList'])->name('r.studentlist');
+    Route::resources([
+        'users' => UserController::class,
+        'groups' => GroupController::class,
+        'sections' => SectionController::class,
+        'data' => MobileDataController::class,
+        'templates' => TemplateController::class,
+    ]);
 
-// Route::get('sms/history', [SmsController::class, 'index'])->name('r.smshistory');
+    Route::get('sections/{section}/list', [SectionController::class, 'GetSectionList'])->name('r.sectionlist');
+    Route::get('data/{groupid}/{sectionid}/list', [MobileDataController::class, 'STDList'])->name('r.studentlist');
 
-Route::get('sms/quick', [SmsController::class, 'ShowQuickSMS'])->name('r.quicksmsshow');
+    // Route::get('sms/history', [SmsController::class, 'index'])->name('r.smshistory');
 
-Route::get('sms/multiple', [SmsController::class, 'ShowMultipleSMS'])->name('r.multiplesmsshow');
+    Route::get('sms/quick', [SmsController::class, 'ShowQuickSMS'])->name('r.quicksmsshow');
 
-Route::get('sms/bulk', [SmsController::class, 'BulkSMSShow'])->name('r.bulksmsshow');
+    Route::get('sms/multiple', [SmsController::class, 'ShowMultipleSMS'])->name('r.multiplesmsshow');
 
-Route::post('sms/quick', [SmsController::class, 'QuickSMS'])->name('r.quicksms');
-Route::post('sms/multiple', [SmsController::class, 'MultipleSMS'])->name('r.multiplesms');
-Route::post('sms/bulk', [SmsController::class, 'BulkSMS'])->name('r.bulksms');
-Route::get('logout', [UserController::class, 'logout'])->name('r.logout');
+    Route::get('sms/bulk', [SmsController::class, 'BulkSMSShow'])->name('r.bulksmsshow');
 
-Route::get('imports', function () {
-    return view('shared.imports');
-})->name('r.imports');
+    Route::post('sms/quick', [SmsController::class, 'QuickSMS'])->name('r.quicksms');
+    Route::post('sms/multiple', [SmsController::class, 'MultipleSMS'])->name('r.multiplesms');
+    Route::post('sms/bulk', [SmsController::class, 'BulkSMS'])->name('r.bulksms');
+    Route::get('logout', [UserController::class, 'logout'])->name('r.logout');
 
-// Data Exports
-Route::prefix('export/xls')->group(function () {
-    Route::get('groups', function () {
-        return Excel::download(new GroupsExport, 'groups.xls', \Maatwebsite\Excel\Excel::XLS);
-    })->name('r.xlsgroups');
+    Route::get('imports', function () {
+        return view('shared.imports');
+    })->name('r.imports');
 
-    Route::get('classes', function () {
-        return Excel::download(new GroupsExport, 'classes.xls', \Maatwebsite\Excel\Excel::XLS);
-    })->name('r.xlsclasses');
+    // Data Exports
+    Route::prefix('export/xls')->group(function () {
+        Route::get('groups', function () {
+            return Excel::download(new GroupsExport, 'groups.xls', \Maatwebsite\Excel\Excel::XLS);
+        })->name('r.xlsgroups');
 
-    Route::get('sections', function () {
-        return Excel::download(new SectionsExport, 'sections.xls', \Maatwebsite\Excel\Excel::XLS);
-    })->name('r.xlssections');
+        Route::get('classes', function () {
+            return Excel::download(new GroupsExport, 'classes.xls', \Maatwebsite\Excel\Excel::XLS);
+        })->name('r.xlsclasses');
 
-    Route::get('students', function () {
-        return Excel::download(new StudentsExport, 'students.xls', \Maatwebsite\Excel\Excel::XLS);
-    })->name('r.xlsstudents');
+        Route::get('sections', function () {
+            return Excel::download(new SectionsExport, 'sections.xls', \Maatwebsite\Excel\Excel::XLS);
+        })->name('r.xlssections');
 
-    Route::get('members', function () {
-        return Excel::download(new MembersExport, 'members.xls', \Maatwebsite\Excel\Excel::XLS);
-    })->name('r.xlsmembers');
-});
+        Route::get('students', function () {
+            return Excel::download(new StudentsExport, 'students.xls', \Maatwebsite\Excel\Excel::XLS);
+        })->name('r.xlsstudents');
 
-Route::prefix('export/csv')->group(function () {
-    Route::get('groups', function () {
-        return Excel::download(new GroupsExport, 'groups.csv', \Maatwebsite\Excel\Excel::CSV, ['Content-Type' => 'text/csv']);
-    })->name('r.csvgroups');
+        Route::get('members', function () {
+            return Excel::download(new MembersExport, 'members.xls', \Maatwebsite\Excel\Excel::XLS);
+        })->name('r.xlsmembers');
+    });
 
-    Route::get('classes', function () {
-        return Excel::download(new GroupsExport, 'classes.csv', \Maatwebsite\Excel\Excel::CSV, ['Content-Type' => 'text/csv']);
-    })->name('r.csvclasses');
+    Route::prefix('export/csv')->group(function () {
+        Route::get('groups', function () {
+            return Excel::download(new GroupsExport, 'groups.csv', \Maatwebsite\Excel\Excel::CSV, ['Content-Type' => 'text/csv']);
+        })->name('r.csvgroups');
 
-    Route::get('sections', function () {
-        return Excel::download(new SectionsExport, 'sections.csv', \Maatwebsite\Excel\Excel::CSV, ['Content-Type' => 'text/csv']);
-    })->name('r.csvsections');
+        Route::get('classes', function () {
+            return Excel::download(new GroupsExport, 'classes.csv', \Maatwebsite\Excel\Excel::CSV, ['Content-Type' => 'text/csv']);
+        })->name('r.csvclasses');
 
-    Route::get('students', function () {
-        return Excel::download(new StudentsExport, 'students.csv', \Maatwebsite\Excel\Excel::CSV, ['Content-Type' => 'text/csv']);
-    })->name('r.csvstudents');
+        Route::get('sections', function () {
+            return Excel::download(new SectionsExport, 'sections.csv', \Maatwebsite\Excel\Excel::CSV, ['Content-Type' => 'text/csv']);
+        })->name('r.csvsections');
 
-    Route::get('members', function () {
-        return Excel::download(new MembersExport, 'members.csv', \Maatwebsite\Excel\Excel::CSV, ['Content-Type' => 'text/csv']);
-    })->name('r.csvmembers');
-});
+        Route::get('students', function () {
+            return Excel::download(new StudentsExport, 'students.csv', \Maatwebsite\Excel\Excel::CSV, ['Content-Type' => 'text/csv']);
+        })->name('r.csvstudents');
 
-// Data Import
-Route::prefix('import')->group(function () {
-    Route::post('groups', [ImportController::class, 'ImportGroups'])->name('r.importgroups');
-    Route::post('classes', [ImportController::class, 'ImportGroups'])->name('r.importclasses');
-    Route::post('sections', [ImportController::class, 'ImportSections'])->name('r.importsections');
-    Route::post('students', [ImportController::class, 'ImportMembers'])->name('r.importstudents');
-    Route::post('members', [ImportController::class, 'ImportMembers'])->name('r.importmembers');
-});
+        Route::get('members', function () {
+            return Excel::download(new MembersExport, 'members.csv', \Maatwebsite\Excel\Excel::CSV, ['Content-Type' => 'text/csv']);
+        })->name('r.csvmembers');
+    });
 
-Route::get('settings', [SettingController::class, 'Settings'])->name('r.settings');
-Route::post('birthdaysettings', [SettingController::class, 'BirthDaySMS'])->name('r.birthdaysettings');
-Route::post('smssettings', [SettingController::class, 'SMSSetting'])->name('r.smssettings');
-// Route::get('test', [SettingController::class, 'Test']);
+    // Data Import
+    Route::prefix('import')->group(function () {
+        Route::post('groups', [ImportController::class, 'ImportGroups'])->name('r.importgroups');
+        Route::post('classes', [ImportController::class, 'ImportGroups'])->name('r.importclasses');
+        Route::post('sections', [ImportController::class, 'ImportSections'])->name('r.importsections');
+        Route::post('students', [ImportController::class, 'ImportMembers'])->name('r.importstudents');
+        Route::post('members', [ImportController::class, 'ImportMembers'])->name('r.importmembers');
+    });
 
-Route::get('packages/{package}/add', [PackageController::class, 'ShowAddPackage'])->name('r.showaddpackage');
-Route::post('packages/{package}/add', [PackageController::class, 'AddPackage'])->name('r.addpackage');
+    Route::get('settings', [SettingController::class, 'Settings'])->name('r.settings');
+    Route::post('birthdaysettings', [SettingController::class, 'BirthDaySMS'])->name('r.birthdaysettings');
+    Route::post('smssettings', [SettingController::class, 'SMSSetting'])->name('r.smssettings');
+    // Route::get('test', [SettingController::class, 'Test']);
 
-Route::prefix('reports')->group(function () {
-    Route::get('todaysummery', [ReportController::class, 'TodaySummery'])->name('r.todaysummery');
-    Route::get('personalizedreport', [ReportController::class, 'PersonalizedReport'])->name('r.personalizedreport');
-});
+    Route::get('packages/{package}/add', [PackageController::class, 'ShowAddPackage'])->name('r.showaddpackage');
+    Route::post('packages/{package}/add', [PackageController::class, 'AddPackage'])->name('r.addpackage');
 
-Route::prefix('reports/pdf')->group(function () {
-    Route::get('todaysummery', [ReportController::class, 'TodaySummeryPDF'])->name('r.todaysummerypdf');
-    Route::get('personalizedreport', [ReportController::class, 'PersonalizedReportPDF'])->name('r.personalizedreportpdf');
+    Route::prefix('reports')->group(function () {
+        Route::get('todaysummery', [ReportController::class, 'TodaySummery'])->name('r.todaysummery');
+        Route::get('personalizedreport', [ReportController::class, 'PersonalizedReport'])->name('r.personalizedreport');
+    });
+
+    Route::prefix('reports/pdf')->group(function () {
+        Route::get('todaysummery', [ReportController::class, 'TodaySummeryPDF'])->name('r.todaysummerypdf');
+        Route::get('personalizedreport', [ReportController::class, 'PersonalizedReportPDF'])->name('r.personalizedreportpdf');
+    });
 });
