3ca89688a7ae09a76d52676d1dcc3fe2f49e35e6
haroon-mahmood-4276 <haroon.mahmood.4276@gmail.com>
2021-08-01 18:14:30 +0500

    Debugger Route Off

--- routes/web.php
+++ routes/web.php
@@ -125,7 +125,7 @@ Route::prefix('import')->group(function () {
 Route::get('settings', [SettingController::class, 'Settings'])->name('r.settings');
 Route::post('birthdaysettings', [SettingController::class, 'BirthDaySMS'])->name('r.birthdaysettings');
 Route::post('smssettings', [SettingController::class, 'SMSSetting'])->name('r.smssettings');
-Route::get('test', [SettingController::class, 'Test']);
+// Route::get('test', [SettingController::class, 'Test']);
 
 Route::get('packages/{package}/add', [PackageController::class, 'ShowAddPackage'])->name('r.showaddpackage');
 Route::post('packages/{package}/add', [PackageController::class, 'AddPackage'])->name('r.addpackage');
