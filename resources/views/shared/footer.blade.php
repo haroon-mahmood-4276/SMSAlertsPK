15e6eba39b4ca28b3b28608655548ab028a7e37a
haroon-mahmood-4276 <haroon.mahmood.4276@gmail.com>
2021-08-02 12:28:08 +0500

    FooterView: Updated

--- resources/views/shared/footer.blade.php
+++ resources/views/shared/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="center-align m-b-30" style="font-size: 15px"><i class="far fa-copyright"></i> All Rights Reserved
-    by SMS Alerts PK Inc. Designed and Developed
+    by <a href="https://iyetech.com/" target="_blank">iyetech.com</a>. Designed and Developed
     <span>&#10084;&#65039;</span> by <a href="https://haroonmahmood.web.app" target="_blank">Haroon
         Mahmood</a>.
 </footer>
