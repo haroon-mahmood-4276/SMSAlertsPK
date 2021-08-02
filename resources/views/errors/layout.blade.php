dc5fd45147f85bf2d55db7c7a19bc0472346e42e
haroon-mahmood-4276 <haroon.mahmood.4276@gmail.com>
2021-08-02 07:19:00 +0500

    Custom Error Messages Pages

--- resources/views/errors/layout.blade.php
+++ resources/views/errors/layout.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon.png') }}">
-    <title>@yield('PageTitle') - SMS Alerts PK</title>
+    <title>@yield('title') - SMS Alerts PK</title>
     <link href="{{ asset('dist/css/style.css') }}" rel="stylesheet">
     <!-- This page CSS -->
     <link href="{{ asset('dist/css/pages/error-pages.css') }}" rel="stylesheet">
