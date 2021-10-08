6a3356ce256575dfb2afdf6f5c38bc58676f32e4
haroon-mahmood-4276 <haroon.mahmood.4276@gmail.com>
2021-10-08 12:41:20 +0500

    Teacher Login Error Log Added

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -251,6 +251,7 @@ class UserController extends Controller
                             $degrees = rad2deg(acos((sin(deg2rad($UserSettings->latitude)) * sin(deg2rad($request->latitude))) + (cos(deg2rad($UserSettings->latitude)) * cos(deg2rad($request->latitude)) * cos(deg2rad($UserSettings->longitude - $request->longitude)))));
 
                             if ((round(($degrees * 111.13384), 2) * 1000) > $UserSettings->radius) {
+                                Log::error("User ID: " . $Teacher->id . "\t Latitude: " . $request->latitude . ", Logitude: " . $request->logitude);
                                 return back()->with('AlertType', 'danger')->with('AlertMsg', 'Sorry! You are not in the premises. You can\'t login.');
                             };
                         } elseif ($request->message == "PERMISSION_DENIED") {
